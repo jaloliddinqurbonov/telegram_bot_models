14d4a0a418df74627e869deeeeaf284479432cc8
jaloliddinqurbonov <qurbonovjaloliddin2003@gmail.com>
2025-07-04 19:19:44 +0500

    salom

--- models.py
+++ models.py
@@ -1,65 +1,36 @@
-class User:
-    def __init__(self, user_data):
-        self.id = user_data.get("id")
-        self.is_bot = user_data.get("is_bot", False)
-        self.first_name = user_data.get("first_name")
-        self.last_name = user_data.get("last_name")
-        self.username = user_data.get("username")
-        self.language_code = user_data.get("language_code")
-
-    def __str__(self):
-        return f"User(id={self.id}, username={self.username}, name={self.first_name} {self.last_name})"
-
-
-class Chat:
-    def __init__(self, chat_data):
-        self.id = chat_data.get("id")
-        self.type = chat_data.get("type")
-        self.title = chat_data.get("title")
-        self.username = chat_data.get("username")
-        self.first_name = chat_data.get("first_name")
-        self.last_name = chat_data.get("last_name")
-
-    def __str__(self):
-        return f"Chat(id={self.id}, type={self.type}, title={self.title})"
-
-
-class Message:
-    def __init__(self, message_data):
-        self.message_id = message_data.get("message_id")
-        self.date = message_data.get("date")
-        self.text = message_data.get("text")
-
-        # Parse user and chat objects
-        if message_data.get("from"):
-            self.from_user = User(message_data["from"])
-        else:
-            self.from_user = None
-
-        if message_data.get("chat"):
-            self.chat = Chat(message_data["chat"])
-        else:
-            self.chat = None
-
-        # TODO: Students need to add parsing for voice, photo, and dice here
-        self.voice = None  # Placeholder - students will implement Voice class
-        self.photo = None  # Placeholder - students will implement Photo class
-        self.dice = None  # Placeholder - students will implement Dice class
-
-    def __str__(self):
-        return (
-            f"Message(id={self.message_id}, text='{self.text}', from={self.from_user})"
+class Update:
+    def __init__(self,update_id :int,message :str):
+        self.update_id=update_id
+        self.message=Message(
+            message_id=message["message_id"],
+            from_user=message["from"],
+            chat=Chat(
+                id=message["chat"]["id"],
+                first_name=message["chat"]["first_name"],
+                username=message["chat"]["username"]  
+            ),
+            date=message["date"],
+            text=message.get("text"),
+            voice=message.get("voice"),
+            document=message.get("document"),   
+            photo=message.get("photo"),
+            video=message.get("video")
         )
 
+class Message:
+    def __init__(self,message_id :int,from_user,chat,date:int,text:str,voice,document,photo,video):
+        self.message_id=message_id
+        self.from_user=from_user
+        self.chat=chat
+        self.date=date
+        self.text=text  
+        self.voice=voice
+        self.document=document
+        self.photo=photo
+        self.video=video
 
-class Update:
-    def __init__(self, update_data):
-        self.update_id = update_data.get("update_id")
-
-        if update_data.get("message"):
-            self.message = Message(update_data["message"])
-        else:
-            self.message = None
-
-    def __str__(self):
-        return f"Update(id={self.update_id}, message={self.message})"
+class Chat:
+    def __init__(self,id,first_name,username):
+        self.id=id
+        self.first_name=first_name
+        self.username=username
--- telegram_bot_starter.py
+++ telegram_bot_starter.py
@@ -1,38 +1,44 @@
-from bot import TelegramBot
+from pprint import pprint
+from bot import Bot
+import os
+from dotenv import load_dotenv
+load_dotenv()
 import time
 
-# Main bot loop
-def main():
-    # Replace with your bot token from @BotFather
-    BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-    bot = TelegramBot(BOT_TOKEN)
-    
-    print("Bot started. Send messages to test...")
-    
-    while True:
-        updates = bot.get_updates()
-        
-        for update in updates:
-            if update.message:
-                print(f"Received: {update}")
-                
-                # Echo back a response for text messages
-                if update.message.text:
-                    bot.send_message(
-                        update.message.chat.id, 
-                        f"You said: {update.message.text}"
-                    )
-                
-                # TODO: Students will add handling for voice, photo, and dice messages here
-                # For example:
-                # if update.message.voice:
-                #     print(f"Voice message received: {update.message.voice}")
-                # if update.message.photo:
-                #     print(f"Photo message received: {update.message.photo}")
-                # if update.message.dice:
-                #     print(f"Dice rolled: {update.message.dice}")
-        
-        time.sleep(1)
+token=os.getenv("token")
+base_url = f"https://api.telegram.org/bot{token}"
 
-if __name__ == "__main__":
-    main()
+
+bot=Bot(token=token,base_url=base_url)
+
+last_update_id = 0
+while True:
+
+    updates=bot.get_update(last_update_id)
+   
+
+    for update in updates:
+
+       
+        last_update_id = update.update_id + 1
+        pprint(update.message)
+        chat_id = update.message.chat.id
+        
+        if update.message.text:
+            text=update.message.text
+            bot.sen_message(chat_id=chat_id,text=text)
+        elif update.message.document:
+            file_id=update.message.document.get("file_id")
+            bot.send_document(chat_id=chat_id,file_id=file_id)
+        elif update.message.voice:
+            voice_id=update.message.voice.get("file_id")
+            bot.send_voice(chat_id=chat_id,voice_id=voice_id)
+        elif update.message.photo:
+            photo_id = update.message.photo[0].get("file_id")
+            bot.send_photo(chat_id=chat_id,photo_id=photo_id)
+        elif update.message.video:
+            video_id = update.message.video.get("file_id")
+            bot.send_video(chat_id=chat_id,video_id=video_id)
+        else:
+            bot.send_dice()
+        time.sleep(1)
